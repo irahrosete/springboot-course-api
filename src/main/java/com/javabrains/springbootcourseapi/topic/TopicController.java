b19b9e46a97844c0523d825936499e208b025aed
irah rosete <irah.rosete@outlook.com>
2021-12-09 00:53:19 +1100

    add topic controller

---
+++ src/main/java/com/javabrains/springbootcourseapi/topic/TopicController.java
@@ -0,0 +1,20 @@
+package com.javabrains.springbootcourseapi.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring Framework Description"),
+                new Topic("java", "Java Framework", "Java Framework Description"),
+                new Topic("javascript", "JavaScript Framework", "JavaScript Framework Description")
+        );
+    }
+}
