265f3dcec87f9340e3d1c83871a3ac65c6ef1b39
irah rosete <irah.rosete@outlook.com>
2021-12-09 00:35:30 +1100

    initial commit

---
+++ src/main/java/com/javabrains/springbootcourseapi/SpringbootCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.javabrains.springbootcourseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCourseApiApplication.class, args);
+	}
+
+}
